4250c99384d6f36a5d1410dc630a4e9ad6f6d07d
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-10 20:05:19 +0600

    added all file update

--- script/data.js
+++ script/data.js
@@ -10,3 +10,17 @@
 // let newPerson = person;
 // person.name = "hero alom";
 // console.log(newPerson);
+
+
+// let person = 'Ratul';
+// console.log(typeof person);
+
+
+
+let person = "gias uddin";
+let newPerson = person;
+
+person = "hero alom";
+
+console.log(newPerson);
+
