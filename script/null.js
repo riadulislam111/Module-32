16e268f07daffa8a58969e561a938e0a7b365baf
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-11 20:49:03 +0600

    added last all file update

--- script/null.js
+++ script/null.js
@@ -53,3 +53,49 @@
 
 
 // console.log(sum(34, 45)) 
+
+////////////////////////////////////////////
+
+
+// let name = null;
+// console.log(typeof name);
+
+
+// console.log(names);
+
+// let name;
+// console.log (name);
+
+// function sum(num1, num2) {
+//     console.log(num1, num2)
+// }
+// sum();
+
+
+
+// function sum(num1, num2) {
+//     console.log(num1, num2)
+// }
+// const result  = sum(10, 20);
+// console.log(result);
+
+
+
+// function sum(num1, num2) {
+//     return;
+// }
+// const result = sum(10, 20);
+// console.log(result);
+
+
+
+// function sum(num1, num2) {
+//     return num1 + num2 ;
+// }
+// const result = sum(10, 20);
+// console.log(result);
+
+
+
+
+
