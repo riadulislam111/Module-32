4a527ba8cf275de47a58c29e30ba5d8fa0442f49
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-11 21:28:33 +0600

    added all file update

--- script/null.js
+++ script/null.js
@@ -99,3 +99,166 @@
 
 
 
+// function sum(num1, num2) {
+
+//     console.log(num1, num2);
+// }
+
+// console.log(sum(10, 20));
+
+
+// let name = null;
+// console.log(typeof name);
+
+
+
+// let age = 25;
+// console.log(typeof age);
+
+
+// let isStudent = true;
+// console.log(typeof isStudent);
+
+
+// let name;
+// console.log(typeof name );
+
+// let city = "Dhaka";
+// console.log(typeof city);
+
+
+// let data = null;
+// console.log(typeof data);
+
+
+
+
+// let myName = "Tamim";
+// console.log(myName);
+
+
+// let score = 90;
+// console.log(score);
+
+
+
+
+
+// let name;
+// console.log(name);
+
+// let city;
+// console.log(city);
+
+
+// let roll;
+// console.log(roll);
+
+
+// let course;
+// console.log(typeof  course);
+
+
+
+
+// function sum(num1 , num2){
+//     // return num1 + num2;
+//     console.log(num1, num2)
+// }
+// sum();
+
+
+
+
+
+// function greet (name) {
+//     console.log("Hello", name);
+// }
+// greet();
+
+// function show (a, b) {
+//     return a + b;
+//     // console.log(a, b)
+// }
+// console.log(show(34,35))
+
+
+
+
+
+// function greet(name){
+//     console.log("Hello", name);
+// }
+
+
+// greet("Rafi");
+// greet();
+
+// function show(a, b) {
+//     console.log(a, b);
+// }
+// show();
+// show(10, 20);
+
+
+
+
+
+// function greet(name) {
+//     console.log("Hello", name);
+// }
+// greet("Mehedi");
+
+
+
+// function show (a, b) {
+//     console.log(a, b);
+// }
+// show("apple", "banana");
+
+
+// function welcome(name, age) {
+//     console.log(`Welcome ${name}, you are ${age} years old.`)
+// }
+
+
+// welcome("Raihan");
+// welcome("Tuli", 25);
+// welcome("Shanto", 18);
+
+
+
+// function sum(num1, num2) {
+//     // return num1 + num2;
+//     console.log(num1, num2)
+// }
+// const result = sum(10, 20);
+// console.log(result);
+
+
+// function printInfo(name) {
+//     console.log("Name:", name);
+// }
+// let res = printInfo("Raju");
+// console.log(res);
+
+
+// function sum(num1, num2){
+//     // return num1 + num2;
+//     return;
+// }
+// const result = sum(10, 20);
+// console.log(result);
+
+
+
+// function giveNothing( ) {
+//     return;
+// }
+// let result = giveNothing();
+// console.log(result);
+
+// function giveSomething() {
+//     return 50;
+// }
+// console.log(giveSomething);
