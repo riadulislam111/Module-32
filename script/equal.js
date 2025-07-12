64a0ed7da523e406499d9aa81bec6842abc35662
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-12 21:07:49 +0600

    added last file update

--- script/equal.js
+++ script/equal.js
@@ -46,3 +46,84 @@
 // }
 
 
+// const a = 10;
+// const b = "10";
+
+// console.log(a == b);
+// console.log(a === b);
+
+
+// "5" = 5
+// false == 0
+// null == undefined
+
+
+
+// const a = 5;
+// const b = "5";
+
+// if(a == b) {
+//     console.log("Double equal: TRUE");
+// }else {
+//     console.log("Double equal: FALSE");
+// }
+
+// if(a === b) {
+//     console.log("Triple equal: TRUE");
+// }else {
+//     console.log("Triple equal: FALSE");
+// }
+
+
+
+// const x = false;
+// const y = 0;
+
+// console.log(x == y);
+// console.log(x === y);
+
+
+
+// const val1 = null;
+// const val2 = undefined;
+
+
+// console.log(val1 == val2);
+// console.log(val1 === val2);
+
+
+// const result = "true";
+// const check = true;
+
+
+// console.log(result == check);
+// console.log(result === check);
+
+
+// const age = "25";
+// const realAge = 25;
+
+// if(age == realAge) {
+//     console.log("Loosely Equal");
+// }else {
+//     console.log("Not Loosely Equal");
+// }
+
+// if (age === realAge) {
+//     console.log("Strictly Equal");
+// }else {
+//     console.log("Not Strictly Equal");
+// }
+
+
+
+
+// console.log(0 == false);
+// console.log(0 === false);
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+
+// console.log("10" == 10);
+// console.log("10" === 10);
