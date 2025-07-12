0c78a18e091eb0db018d44d929a2304a8a894d78
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-12 20:46:38 +0600

    added last all file update

--- script/equal.js
+++ script/equal.js
@@ -32,3 +32,15 @@
 //     console.log("NOO this is not Equal")
 // }
 
+////////////////
+
+
+const num1 = 10;
+const num2 = 10;
+
+if (num1 === num2) {
+    console.log("this is Equal value")
+}
+else{
+    console.log("NOO this is not Equal")
+}
