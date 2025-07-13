c37a413bc55c66dd627a97e5e5df4dbd3886293b
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-14 04:02:05 +0600

    added all file update

--- script/scope.js
+++ script/scope.js
@@ -1,122 +1,206 @@
-// // const resultTop = "test";
+// // // const resultTop = "test";
 
 
-// // // console.log(resultTop);
-
-
-
+// // // // console.log(resultTop);
 
 
 
 
 
 
-// // function sum (num1, num2){
-// //     const result = num1 + num2;
+
+
+
+// // // function sum (num1, num2){
+// // //     const result = num1 + num2;
 
     
-
-// //     if(true){
-// //         var result2 = num1 - num2;
-// //     }
-
-// //     // console.log(result2)
-// // }
-
-
-// // sum (10,20);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // // function sum (num1, num2){
-  
-// // //     const result = num1 + num2;
-
-// // //     console.log(result)
-// // // }
-
-
-// // // sum (10,20);
-
-
-// // // function sum (num1, num2){
-// //   //  var result2;// behind the seen
-
-// // //     const result = num1 + num2;
-
-// // //     console.log(result2);
 
 // // //     if(true){
 // // //         var result2 = num1 - num2;
 // // //     }
 
-    
+// // //     // console.log(result2)
 // // // }
 
 
 // // // sum (10,20);
 
 
-// // // hoisting//
-
-// // // var output;
-
-// // // console.log(output);
-// // // var output = "BD";
-
-
-
-// //////////////
-
-// // function sum(num1, num2) {
-// //   const result = num1  + num2;
-// // // console.log(result)
-
-
-// // if (ture) {
-// //   // const result2 = num1 - num2;
-// //   var result2 = num1 - num2;
-
-
-// // }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // function sum (num1, num2){
+  
+// // // //     const result = num1 + num2;
+
+// // // //     console.log(result)
+// // // // }
+
+
+// // // // sum (10,20);
+
+
+// // // // function sum (num1, num2){
+// // //   //  var result2;// behind the seen
+
+// // // //     const result = num1 + num2;
+
+// // // //     console.log(result2);
+
+// // // //     if(true){
+// // // //         var result2 = num1 - num2;
+// // // //     }
+
+    
+// // // // }
+
+
+// // // // sum (10,20);
+
+
+// // // // hoisting//
+
+// // // // var output;
+
+// // // // console.log(output);
+// // // // var output = "BD";
+
+
+
+// // //////////////
+
+// // // function sum(num1, num2) {
+// // //   const result = num1  + num2;
+// // // // console.log(result)
+
+
+// // // if (ture) {
+// // //   // const result2 = num1 - num2;
+// // //   var result2 = num1 - num2;
+
+
+// // // }
+
+
+
+
+
+// // // // console.log(result);
+// // // }
+// // // sum(12, 34);
 
 // // // console.log(result);
-// // }
-// // sum(12, 34);
 
+
+
+
+// // function sum(num1, num2) {
+  
+// //   const 
+// // }
+
+
+// //////////
+
+
+// const resultTop = "test";
+// function sum(num1, num2) {
+//   const result = num1 + num2;
+
+// // console.log(result);
+// // console.log(result2);
+// if(true)  {
+//   // const result2 = num1 - num2;
+//  var result2 = num1 - num2;
+
+// // console.log(result2);
+// }
+// // console.log(result2);
+
+
+
+
+// }
+// sum(10, 20);
 // // console.log(result);
 
 
 
 
-// function sum(num1, num2) {
-  
-//   const 
+
+// // // var output;
+// // console.log(output);
+// // var output = "BD"; 
+// // // const output = "BD"; 
+
+
+// // console.log(resultTop);
+
+
+// // if(true) {
+// //   const result2 = num1 - num2;
+
+// //   console.log(result2);
+// // }
+
+
+
+// function testVarScope() {
+//   if(true) {
+//     var a = 5;
+    
+//   }
+//   console.log(a);
 // }
 
-
-//////////
-
+// testVarScope();
 
 
+
+// function testLetScope() {
+//   if(true) {
+//     let b= 10;
+    
+//   }
+//   console.log(b);
+// }
+// testLetScope();
+
+
+// console.log(c);
+// var c = "hello";
+
+
+// // console.log(c);
+// // const d = "hello";
+
+// console.log(d);
+// const d = "hello";
+
+
+
+function checkScope() {
+  if(true) {
+    var x = 100;
+    let y = 200;
+  }
+  console.log(x);
+  console.log(y);
+}
+checkScope();
