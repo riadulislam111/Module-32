c5af17dd8298960fe51519c462f81e015200e549
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-15 20:08:38 +0600

    added  all file update

--- script/arguments.js
+++ script/arguments.js
@@ -70,3 +70,97 @@
 // console.log(rat);
 
 
+////////////
+
+
+// function sum(a, b, c) {
+//     console.log(arguments)
+// }
+
+
+// sum(10, 20, 30, 40, 50, 60);
+
+
+// function sum(a, b, c) {
+//     const newArg = [...arguments];
+ 
+//     console.log(newArg)
+// }
+
+// sum(10, 20, 30, 40);
+
+// function sum(a, b, c) {
+//     console.log(arguments)
+// }
+
+
+// console.log(sum.length);
+
+
+// let name = "masud";
+
+// function test(text) {
+//     text = "korim";
+
+// }
+
+// test(name);
+// console.log(name);
+
+
+// let a = 10;
+// let b = 20;
+
+// function sum(num1, num2) {
+//     num1 = 50;
+// }
+
+
+
+// sum(a, b);
+
+// console.log(a);
+
+
+
+// const person = { name: "gias", age :10}
+
+
+// function sum(obj) {
+//     obj.name = "hero laom"
+// }
+
+
+// sum(person);
+// console.log(person);
+
+
+// const person = { name: "rakib", age: 24};
+
+// function sum(obj) {
+//     obj.name = "hero laom";
+// }
+
+// sum(person);
+// console.log(person);
+
+
+// const person = { name: "gias", age: 10};
+
+// function changeName(obj) {
+//     obj.name = "hero aloim";
+// }
+
+// changeName(person);
+//  console.log(person);
+
+
+// const person = { name: "masud", age: 23};
+
+
+// function changeName(obj) {
+//     obj.name = "hero alom";
+// }
+
+// changeName(person);
+// console.log(person);
