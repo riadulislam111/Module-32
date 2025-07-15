636170f784fd0d18d9d58beb3d5d0739ddec447d
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-15 19:16:44 +0600

    added all fil update

--- script/closure.js
+++ script/closure.js
@@ -89,9 +89,59 @@
 
 
 
-function ghotok(patri) {
-    console.log("ami ki call korbo", patri)
-}
+// function ghotok(patri, name) {
+//     // console.log(patri)
+//     patri(name);
+// }
 
-const patri = "rahima";
-ghotok(patri);
+// const patri1 = function(name) {
+//     console.log("okey ami paici1", name)
+// }
+// const patri2 = function(name) {
+//     console.log("okey ami paici2", name)
+// }
+// const patri3 = function(name) {
+//     console.log("okey ami paici3", name)
+// }
+// const patri4 = function(name) {
+//     console.log("okey ami paici4", name)
+// }
+
+// ghotok(patri1, "hero alom");
+// ghotok(patri2, "khan");
+// ghotok(patri3, "mash");
+
+
+
+// function ghotok(patri, name){
+// patri(name);
+// }
+
+
+// const patri1 = function(name) {
+//     console.log("okey ami paici1", name)
+// }
+
+
+// const patri2 =function(name){
+//     console.log("okey ami paici2", name)
+// }
+
+// const patri3 = function (name) {
+//     console.log("okey ami paici3", name)
+// }
+
+
+// // ghotok(patri1, "hero alom");
+// // ghotok(patri2, "Khan");
+// // ghotok(patri3, "mash");
+
+
+
+// ghotok(patri1, "shakin");
+// ghotok(patri2, "sourove");
+// ghotok(patri3, "rahj");
+// ghotok(patri1, "rolando");
+
+
+
