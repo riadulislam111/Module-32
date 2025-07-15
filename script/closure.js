b7e0ebec749f963a9fe369ae1e0f5527c79acfed
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-15 18:55:18 +0600

    added all fil update

--- script/closure.js
+++ script/closure.js
@@ -1,36 +1,97 @@
-//  function sum(){
+// //  function sum(){
+// //     let counter = 0;
+// //     counter ++;
+// //     console.log(counter)
+// //  }
+
+// //  sum ()
+// //  sum()
+// //  sum()
+
+
+
+// // function sum(){
+// //     let counter = 0;
+// //     return function (){
+// //         counter ++;
+// //         console.log(counter)
+// //     }
+    
+// // }
+
+
+// // const result=sum()
+// // result()
+// // result()
+// // result()
+// // result()
+
+// // const result2=sum()
+// // result2()
+// // result2()
+// // result2()
+
+
+
+// // result()
+
+// ///////////////////
+
+// // let counter = 0;
+
+// function sum() {
 //     let counter = 0;
-//     counter ++;
-//     console.log(counter)
-//  }
+    
+//     return function () {
 
-//  sum ()
-//  sum()
-//  sum()
+//      counter ++;
+//      console.log(counter);
 
 
-
-// function sum(){
-//     let counter = 0;
-//     return function (){
-//         counter ++;
-//         console.log(counter)
 //     }
     
-// }
-
-
-// const result=sum()
-// result()
-// result()
-// result()
-// result()
-
-// const result2=sum()
-// result2()
-// result2()
-// result2()
+//     }
 
 
 
-// result()
+
+//     const result = sum();
+//     result();
+//     result();
+//     result();
+//     result();
+
+
+//     const result2 = sum();
+//     result2();
+//     result2();
+//     result2();
+//     result2();
+
+
+
+
+
+// // console.log(result);
+// // counter = 11;
+// // sum();
+// // sum();
+// // sum();
+// // sum();
+// // sum();
+
+
+
+
+///////////
+
+
+
+
+
+function ghotok(patri) {
+    console.log("ami ki call korbo", patri)
+}
+
+const patri = "rahima";
+ghotok(patri);
