f7cd94cd60f2360481b56fd05f2dbaeac99465c0
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-16 18:01:19 +0600

    addeda all file update

--- script/summary.js
+++ script/summary.js
@@ -14,4 +14,111 @@
 
 // // 6) Define block scope and global scope. 
 // console.log(name);
-// var name = "Riadul";
+// // var name = "Riadul";
+// 7) What is hoisting in JavaScript?
+
+// 8) How to use the JavaScript callback function?
+
+// 9) Explain closure in JavaScript
+
+// 10) Explain passed by value and passed by reference.
+
+
+
+
+//////
+
+
+
+// let num1 = 10;
+// let num2 = num1;
+
+// num1 = 20;
+
+// console.log(num1);
+// console.log(num2);
+
+
+
+
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+
+// arr1.push(4);
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+// let a = 5;
+// let b = a;
+
+// a = 10;
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+// let obj1 = { name: "Alice"};
+// let obj2 = obj1;
+
+// obj1.name = "Bob";
+
+// console.log(obj1.name);
+// console.log(obj2.name);
+
+
+
+
+// if(false) { console.log("This is falsy");}
+// if (0) { console.log("This is falsy!");}
+// if("") { console.log("this is falsy");}
+// if(null) { console.log("this is falsy");}
+// if(undefined) { console.log("This is falsy");
+// }
+
+// if(NaN) { console.log("This is falsy");}
+
+
+
+// if(true) { console.log("This is truthy");}
+// if(1) { console.log("This is truthy");}
+// if ("hello") { console.log("this is truthy");
+// }
+// if({}) {console.log("this is trughty");}
+// if([]) {console.log("this is truthy");}
+
+
+
+// let value1 = 0;
+// if(value1) {
+//     console.log("This is truthy");
+// }else{
+//     console.log("this is falsy");
+// }
+
+
+// let value2 = "Hello";
+// if(value2) {
+//     console.log("this is truthy");
+// }else{
+//     console.log("this is falsy");
+// }
+
+
+
+// if ("Hello") {
+//     console.log("This is truthy");
+// }
+
+// if(0) {
+//     console.log("This is truthu");
+// }else{
+//      console.log("this is falsy");
+// }
+
+
+
+
