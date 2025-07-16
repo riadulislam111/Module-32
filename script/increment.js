41ff756f8f342ca884724bf37f91b5a767dce0f8
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-16 17:22:15 +0600

    added all file update

--- script/increment.js
+++ script/increment.js
@@ -25,3 +25,80 @@
 
 
 
+//////////////
+
+
+// let age = 10;
+
+// // console.log(++age);
+// // console.log(age++);
+// // console.log(age);
+
+// console.log(--age);
+// console.log(age--);
+// console.log(age);
+
+
+// let age = 10;
+// console.log(++age);
+// console.log(age);
+
+
+
+// let age = 10;
+// console.log(age++);
+// console.log(age);
+
+
+
+
+// let age = 10;
+// console.log(--age);
+// console.log(age);
+
+
+
+// let age = 10;
+// console.log(age--);
+// console.log(age);
+
+
+
+
+
+
+// let age = 5;
+// console.log(++age);
+// console.log(age);
+
+
+
+
+// let age = 10;
+// console.log(age++);
+// console.log(age);
+
+
+
+// let age = 20;
+// console.log(--age);
+// console.log(age);
+
+
+
+
+// let age = 25;
+// console.log(age--);
+// console.log(age);
+
+
+
+// let age = 30;
+// console.log(++age);
+// console.log(age++);
+// console.log(age); 
+
+
+
+
+
