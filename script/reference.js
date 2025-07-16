3c959dfdfebbac6ff83b9c2a22fa6e07a725f74a
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-16 17:00:22 +0600

    added all file update

--- script/reference.js
+++ script/reference.js
@@ -49,3 +49,66 @@
 // const person = { name: "Karim"};
 // changeName(person);
 // console.log("functoion er baire:", person.name);
+
+
+// function changeName(num) {
+//     num = num + 10;
+//     console.log("function er vitore:", num);
+// }
+
+// const myNumber = 10;
+// changeName(myNumber);
+// console.log("function r baire:", myNumber);
+
+
+// function changeName(person) {
+//     person.name = "Rahim";
+//     console.log("function er vitore:", person.name);
+// }
+
+// const person = { name: "Karim"};
+// changeName(person);
+// console.log("function er baire:", person.name);
+
+
+
+
+// let number = 5;
+// function changeNumber(num) {
+//     num = num * 2;
+//     console.log(num);
+// }
+// changeNumber(number);
+// console.log(number);           
+
+
+
+// let person = { name: "Ali"};
+// function changeName(p) {
+//     p.name = "Rana";
+// }
+
+// changeName(person);
+// console.log(person.name);
+
+
+
+// let person = { name: "John" };
+// function changeName(p) {
+//     p = { name: "Tariq"};
+// }
+
+// changeName(person);
+// console.log(person.name);
+
+
+
+// let arr = [1, 2, 3];
+// function changeArry(a) {
+//     a.push(4);
+// }
+
+// changeArry(arr);
+// console.log(arr);
+
+
